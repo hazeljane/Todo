a45d5c219a519a60577849a858a090988ef10c60
hazeljane <gunohazeljane@gmail.com>
2025-02-27 08:10:39 +0700

    Initial commit

---
+++ my-jsx-project/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './styles/main.scss'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, JSX is Working!</h1>
+    </div>
+  );
+}
+
+export default App;
+
